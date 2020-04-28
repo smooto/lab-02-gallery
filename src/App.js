99a000c51a058ccfed6cfd51a70f2acbb1015500
smooto <melissasmoot@live.com>
2020-04-28 15:50:05 -0700

    add static layout for blocking out styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header>
+        <h1>My Horny Friends!</h1>
       </header>
-    </div>
+      <main>
+        <section>
+          <select>
+            <option>Option 1</option>
+            <option>Option 2</option>
+            <option>Option 3</option>
+          </select>
+          <ul>
+            <li>
+              <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" />
+              <p>UniWhal</p>
+              <p>A unicorn and a narwhal nuzzling their horns</p>
+            </li>
+          </ul>
+        </section>
+      </main>
+      <footer>
+        M. Smoot 2020
+      </footer>
+    </>
   );
 }
 
