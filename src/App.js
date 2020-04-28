d8d800d804697147a4b2bb5c7dd83b43c46a2bcd
smooto <melissasmoot@live.com>
2020-04-28 16:27:35 -0700

    add basic css layout

--- src/App.js
+++ src/App.js
@@ -9,16 +9,27 @@ function App() {
       </header>
       <main>
         <section>
+          <p>Filter by number of horns:</p>
           <select>
             <option>Option 1</option>
             <option>Option 2</option>
             <option>Option 3</option>
           </select>
           <ul>
-            <li>
-              <img src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" />
-              <p>UniWhal</p>
-              <p>A unicorn and a narwhal nuzzling their horns</p>
+            <li className="card-wrapper">
+              <img className="card-img" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" />
+              <p className="card-title">UniWhal</p>
+              <p className="card-description">A unicorn and a narwhal nuzzling their horns</p>
+            </li>
+            <li className="card-wrapper">
+              <img className="card-img" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" />
+              <p className="card-title">UniWhal</p>
+              <p className="card-description">A unicorn and a narwhal nuzzling their horns</p>
+            </li>
+            <li className="card-wrapper">
+              <img className="card-img" src="http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg" alt="A unicorn and a narwhal nuzzling their horns" />
+              <p className="card-title">UniWhal</p>
+              <p className="card-description">A unicorn and a narwhal nuzzling their horns</p>
             </li>
           </ul>
         </section>
